ecc5e99abf5d377c12f934c9a61c66802d4e5859
PriCampos <84priscila.campos@gmail.com>
2017-10-26 17:28:31 -0200

    correções na página about-me

--- www/about-me.php
+++ www/about-me.php
@@ -33,35 +33,33 @@
         
         <section class="about-me">
             <h1 class="title-about-me">A Little Bit About Me...</h1>
-            <ul class="ul-about-me">
-                <li class="li-about-me" id="li-img-about-me">
-                    <img class="img-about-me" src="http://localhost:81/pricampos.blog.br/www/galeria/banner/priscila.png" />
-                    <div class="div-network">
-                        <h4><a class="a-network" href="https://www.facebook.com/priscila.campos.752" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i>Facebook</a></h4>
-                        <h4><a id="a-network" href="https://www.linkedin.com/in/priscila-campos-7b4653134/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true" ></i>Linkedin</a></h4>
-                        <h4><a class="a-network" href="https://github.com/PriCampos" target="_blank"><i class="fa fa-github-square" aria-hidden="true" >GitHub</i></a></h4>
-                    </div>
-                </li>
-                <li class="li-about-me" id="li-content-about-me">
-                    <h1>Priscila Campos</h1>
-                    
-                    <h4><i class="fa fa-map-marker" aria-hidden="true"></i> Belo Horizonte, MG</h4>
-                    
-                    <p>Sou graduanda do curso de Engenharia da Computação, no Centro Universitário UNA. E desde 2012, atuo na área de Teste de Software.</p>
-
-                    <p>Meus hobbies favoritos são desenhar, crochetar, escrever frases que me marcam e alguns pensamentos, correr, pedalar, curti os serumaninhos de 4 patas, observar a natureza, ouvir o filósofo Pe. Fábio de Melo e assistir séries e filmes.</p>
-
-                    <p>Busco aprendizado, me tornar uma das melhores profissionais da minha área e ter uma ideia que melhore a vida de outras pessoas, algo que dê desígnio aos estudos.</p>
-                    
-                    <p>Acredito no poder de expandir as nossas ideias por meio do desenvolvimento conjunto e propósitos pluralistas.</p>
             
-                    <p>Decidi fazer o blog, porquê quando compartilho um estudo, percebo que me esforço mais para absorver o conteúdo, tenho mais atenção aos detalhes diante do desafio de reescrever um objeto de estudo. Outras vantagens é obter feedback e melhorar a escrita. Também considero válido um acervo on-line do que tenho estudado, seja para uso próprio e para outros que buscam um conteúdo com uma outra percepção ou linguagem.</p>
-
-                    <p> Muito obrigada, por em algum momento, fazer parte disso! </p>
+            <h1>Priscila Campos</h1>
             
-                    <p>Abraços!</p>
-                </li>
-            </ul>
+            <h4><i class="fa fa-map-marker" aria-hidden="true"></i> Belo Horizonte, MG</h4>
+
+           
+            <p>Sou graduanda do curso de Engenharia da Computação, no Centro Universitário UNA. E desde 2012, atuo na área de Teste de Software.</p>
+
+            <p>Meus hobbies favoritos são desenhar, crochetar, escrever frases que me marcam e alguns pensamentos, correr, pedalar, curti os serumaninhos de 4 patas, observar a natureza, ouvir o filósofo Pe. Fábio de Melo e assistir séries e filmes.</p>
+
+            <p>Busco aprendizado, me tornar uma das melhores profissionais da minha área e ter uma ideia que melhore a vida de outras pessoas, algo que dê desígnio aos estudos.</p>
+                    
+            <p>Acredito no poder de expandir as nossas ideias por meio do desenvolvimento conjunto e propósitos pluralistas.</p>
+            
+            <p>Decidi fazer o blog, porquê quando compartilho um estudo, percebo que me esforço mais para absorver o conteúdo, tenho mais atenção aos detalhes diante do desafio de reescrever um objeto de estudo. Outras vantagens é obter feedback e melhorar a escrita. Também considero válido um acervo on-line do que tenho estudado, seja para uso próprio e para outros que buscam um conteúdo com uma outra percepção ou linguagem.</p>
+
+            <p> Muito obrigada, por em algum momento, fazer parte disso! </p>
+            
+            <p>Abraços!</p>
+                    
+            <img class="img-about-me" src="http://localhost:81/pricampos.blog.br/www/galeria/banner/priscila.png" />
+            
+            <div class="div-network">
+                <h4><a class="a-network" href="https://www.facebook.com/priscila.campos.752" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i>Facebook</a></h4>
+                <h4><a id="a-network" href="https://www.linkedin.com/in/priscila-campos-7b4653134/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true" ></i>Linkedin</a></h4>
+                <h4><a class="a-network" href="https://github.com/PriCampos" target="_blank"><i class="fa fa-github-square" aria-hidden="true" >GitHub</i></a></h4>
+            </div>     
         </section> 
    
         <footer>
