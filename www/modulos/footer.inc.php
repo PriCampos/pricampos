90cf0089a921b37fc3f1d7a154cb9f2ec4afdd57
PriCampos <84priscila.campos@gmail.com>
2017-11-16 14:09:04 -0200

    correção do rodapé, o mesmo passa a ficar no final da página, indiferente do contéudo

--- www/modulos/footer.inc.php
+++ www/modulos/footer.inc.php
@@ -1,22 +1,24 @@
-<div class="footer">
-    <ul class="footer-resize">
-        <li class="footer-text">
-            <p> &lt;?php </p>
-            <p> &nbsp; &nbsp; echo "Na minha máquina funciona."; </p>
-            <p> ?&gt; </p>
-        </li>
-        <li class="footer-return-top">
-            <label for="btn-return-top">
-                <a class="a-footer-return" href="#top">
-                    <i class="fa fa-grav fa-2x" aria-hidden="true"></i> Voltar ao Topo
-                </a>
-            </label>
-        </li>
-    </ul>
-</div>
+<div class="footer-main">
+    <div class="footer">
+        <ul class="footer-resize">
+            <li class="footer-text">
+                <p> &lt;?php </p>
+                <p> &nbsp; &nbsp; echo "Na minha máquina funciona."; </p>
+                <p> ?&gt; </p>
+            </li>
+            <li class="footer-return-top">
+                <label for="btn-return-top">
+                    <a class="a-footer-return" href="#top">
+                        <i class="fa fa-grav fa-2x" aria-hidden="true"></i> Voltar ao Topo
+                    </a>
+                </label>
+            </li>
+        </ul>
+    </div>
     
-<div class="footer-copyright">
-    <a href="/pricampos.blog.br/www/contact.php">
-        <p>@2017 Contate-me | pricampos.blog.br </p>
-    </a>
+    <div class="footer-copyright">
+        <a href="/pricampos.blog.br/www/contact.php">
+            <p>@2017 Contate-me | pricampos.blog.br </p>
+        </a>
+    </div>
 </div>
